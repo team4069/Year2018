81377ea924bbe5dd0cb1363642afe4fd7fae1587
brendon-ai <brendon-m@outlook.com>
2018-01-19 16:56:16 -0500

    Added GRIP vision code for detecting yellow box

---
+++ src/main/java/frc/team4069/robot/vision/ArmCameraThread.java
@@ -0,0 +1,12 @@
+package frc.team4069.robot.vision;
+
+// The thread that manages the camera on the arm that allows the robot to position itself in order
+// to pick up a power cube with vision
+public class ArmCameraThread implements Runnable {
+
+    // Main loop
+    @Override
+    public void run() {
+
+    }
+}
