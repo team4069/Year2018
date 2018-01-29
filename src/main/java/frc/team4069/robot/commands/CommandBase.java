7cbdbf9f667d7d67fc7b909852c951e415e986a6
Unknown <redrield@gmail.com>
2018-01-29 13:38:32 -0500

    Get rid of deleted subsystem references

--- src/main/java/frc/team4069/robot/commands/CommandBase.java
+++ src/main/java/frc/team4069/robot/commands/CommandBase.java
@@ -1,7 +1,6 @@
 package frc.team4069.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.team4069.robot.subsystems.ClimberHookSubsystem;
 import frc.team4069.robot.subsystems.DriveBaseSubsystem;
 import frc.team4069.robot.subsystems.ElevatorSubsystem;
 
@@ -10,14 +9,12 @@ public abstract class CommandBase extends Command {
 
     // Instances of each of the subsystems
     static DriveBaseSubsystem driveBase;
-    static ClimberHookSubsystem climberHook;
     static ElevatorSubsystem elevator;
 
     // An function that handles initialization of subsystems
     public static void init() {
         // Get the singleton instance of each of the subsystems
         driveBase = DriveBaseSubsystem.getInstance();
-        climberHook = ClimberHookSubsystem.getInstance();
         elevator = ElevatorSubsystem.INSTANCE;
     }
 }
