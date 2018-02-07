2fade3c0510fa0113bfe2544d6d886dc1dda3944
brendon-ai <brendon-m@outlook.com>
2018-02-07 14:41:06 -0500

    Add command at autonomous init

--- src/main/java/frc/team4069/robot/commands/autonomous/DriveToSwitchCommand.java
+++ src/main/java/frc/team4069/robot/commands/autonomous/DriveToSwitchCommand.java
@@ -4,13 +4,13 @@ import edu.wpi.first.wpilibj.DriverStation;
 import frc.team4069.robot.commands.CommandBase;
 
 // Drive from the starting position to the switch in autonomous mode
-class DriveToSwitch extends CommandBase {
+public class DriveToSwitchCommand extends CommandBase {
 
     // The robot's starting position for autonomous mode
     private StartingPosition startingPosition = StartingPosition.RIGHT;
 
     // Constructor, used to claim subsystems
-    DriveToSwitch() {
+    public DriveToSwitchCommand() {
         // Claim exclusive use of the drive base
         requires(driveBase);
     }
